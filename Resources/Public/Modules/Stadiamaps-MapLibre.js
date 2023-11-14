d5b42de9e2ed6d63be9fe0be21a4d6c1f12c3e1a
Jon Uhlmann <account@uhlmann.pro>
2023-11-14 12:16:27 +0100

    Upgrade: MapLibre and Protomaps

--- Resources/Public/Modules/Stadiamaps-MapLibre.js
+++ Resources/Public/Modules/Stadiamaps-MapLibre.js
@@ -1,3 +1,3 @@
-import{b as i}from"./chunk-WIRDCQIQ.js";import"./chunk-DN2P5LLP.js";i();
+import{b as i}from"./chunk-4U67YDI4.js";import"./chunk-DN2P5LLP.js";i();
 /*! For license information please see Stadiamaps-MapLibre.js.LEGAL.txt */
 //# sourceMappingURL=Stadiamaps-MapLibre.js.map
