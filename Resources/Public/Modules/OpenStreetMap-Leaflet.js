6ef3db9a3b1ed83e6c23330e31b4d5f5753ceeee
Jon Uhlmann <account@uhlmann.pro>
2023-12-26 23:40:46 +0100

    Upgrade: Packages

--- Resources/Public/Modules/OpenStreetMap-Leaflet.js
+++ Resources/Public/Modules/OpenStreetMap-Leaflet.js
@@ -1,3 +1,3 @@
-import{b as i}from"./chunk-CLY5IIZ3.js";import"./chunk-YWXEH3IL.js";i();
+import{b as i}from"./chunk-73CT22NR.js";import"./chunk-YWXEH3IL.js";i();
 /*! For license information please see OpenStreetMap-Leaflet.js.LEGAL.txt */
 //# sourceMappingURL=OpenStreetMap-Leaflet.js.map
--- Resources/Public/Modules/Stadiamaps-MapLibre.js
+++ Resources/Public/Modules/Stadiamaps-MapLibre.js
@@ -1,3 +1,3 @@
-import{b as i}from"./chunk-3XY7GH3E.js";import"./chunk-YWXEH3IL.js";i();
+import{b as i}from"./chunk-QKABKDJQ.js";import"./chunk-YWXEH3IL.js";i();
 /*! For license information please see Stadiamaps-MapLibre.js.LEGAL.txt */
 //# sourceMappingURL=Stadiamaps-MapLibre.js.map
